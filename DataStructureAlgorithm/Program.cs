c9baedb05ff1ad39b1df9d19a0e7c57023aef0ba
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-28 15:53:29 +0530

    [Mukesh] Add: Implement the Binary Search Program

--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataStructureAlgorithm;
+
+public class Program
+{
+    static string searchFilePath = @"D:\Problem statemets\DataStructureAlgorithm\DataStructureAlgorithm\Files\Search.txt";
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Data Structure\n");
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("Enter a option \n1.BinarySearch \n2.Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    BinarySearch search = new BinarySearch();
+                    search.ReadTextFileAndSearch(searchFilePath);
+                    break;
+                case 2:
+                    flag = false;
+                    break;
+            }
+        }
+
+
+    }
+}
