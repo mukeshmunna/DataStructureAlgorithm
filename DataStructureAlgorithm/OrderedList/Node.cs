421bf6edb9d55ba84fc115cf46b715232e9ea569
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-08-01 22:23:38 +0530

    [Mukesh] Add : Ordered List

---
+++ DataStructureAlgorithm/OrderedList/Node.cs
@@ -0,0 +1,19 @@
+﻿using DataStructureAlgorithm.OrderedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm.OrderedList
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
